0ea6e2f6bc0bd8885172112420719ea269fdb234
Amanda Savluchinske <amandasavluchinske@gmail.com>
2020-03-11 23:13:10 -0300

    Task1 done

--- Task1.py
+++ Task1.py
@@ -18,3 +18,24 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+def get_all_phone_numbers():
+    phones_list = []
+    with open('texts.csv', 'r') as f:
+        reader = csv.reader(f)
+        texts = list(reader)
+        for row in texts:
+            phones_list.extend([row[0], row[1]])
+
+    with open('calls.csv', 'r') as f:
+        reader = csv.reader(f)
+        calls = list(reader)
+        for row in calls:
+            phones_list.extend([row[0], row[1]])
+
+    return phones_list
+
+def print_unique_phones_count(unique_phones_list):
+    print(f"There are {len(unique_phones_list)} different telephone numbers in the records.")
+
+print_unique_phones_count(list(set(get_all_phone_numbers())))
